76ac1e0f572ca8c3b137f1b56736eb0d4012aae2
Rodrigo Gonçalves <rodrigogoncalveess@gmail.com>
2019-09-29 12:24:00 -0300

    adicionado api de nóticias (de qualquer jeito)

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 /**
  * @author Rodrigo Gonçalves
- * @description arquivo principal do bot responsavel por validar as mensagens 
+ * @description arquivo principal do bot responsavel por validar as mensagens
  * recebidas enquanto a execução fica por conta da classe Acao
  */
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const Bot = new Discord.Client();
-const Acoes = require('./src/classes/Acoes.js');
+const Acoes = require("./src/classes/Acoes.js");
 const a = new Acoes();
 // TODO - forma mais eficiente para buscar comandos
-const {
-    prefix,
-    comandos
-} = require('./src/data/comandos.json');
-const token = require('./src/data/token.json');
+const { prefix, comandos } = require("./src/data/comandos.json");
+const token = require("./src/data/token.json");
 
-Bot.on('ready', () => {
-    a.logger(`Iniciando >>> ${Bot.user.tag} id: ${Bot.user.id} <<<`);
+Bot.on("ready", () => {
+  a.logger(`Iniciando >>> ${Bot.user.tag} id: ${Bot.user.id} <<<`);
 });
 
-
-Bot.on('message', message => {
-    let cont = message.content.toLocaleLowerCase();
-    if (cont.startsWith(prefix)) {
-        if (cont.startsWith(prefix + 'ajuda')) {
-            a.logger(`Comando ajuda chamdo por ${message.author.username}`);
-            a.ajuda(message, prefix, comandos);
-        } else if (cont.startsWith(prefix + 'build')) {
-            a.logger(`Comando build chamdo por ${message.author.username}`);
-            a.build(message);
-        } else if (cont.startsWith(prefix + 'rank')) {
-            if (cont.split(' ')[1]) {
-                a.logger(`Comando rank chamdo por ${message.author.username}`);
-                a.rankIndividual(message);
-            } else {
-                a.logger(`Comando rank chamdo por ${message.author.username}`);
-                a.ranking(message);
-            }
-        }
+Bot.on("message", message => {
+  let cont = message.content.toLocaleLowerCase();
+  if (cont.startsWith(prefix)) {
+    if (cont.startsWith(prefix + "ajuda")) {
+      a.logger(`Comando ajuda chamdo por ${message.author.username}`);
+      a.ajuda(message, prefix, comandos);
+    } else if (cont.startsWith(prefix + "build")) {
+      a.logger(`Comando build chamdo por ${message.author.username}`);
+      a.build(message);
+    } else if (cont.startsWith(prefix + "rank")) {
+      if (cont.split(" ")[1]) {
+        a.logger(`Comando rank chamdo por ${message.author.username}`);
+        a.rankIndividual(message);
+      } else {
+        a.logger(`Comando rank chamdo por ${message.author.username}`);
+        a.ranking(message);
+      }
     }
+  } else if (cont.startsWith(prefix + "news")) {
+    a.logger(`Comando news chamdo por ${message.author.username}`);
+    a.pegaNoticia(message);
+  }
 });
 
 Bot.login(token.token);
--- src/classes/Acoes.js
+++ src/classes/Acoes.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require("request");
 
 /**
  * @author Rodrigo Gonçalves
@@ -6,265 +6,284 @@ const request = require('request');
  * aceitos pelo bot
  */
 class Acoes {
+  /**
+   * @description Um logger simples que imprime no terminal ações
+   * realizadas pelo bot
+   * @param {String} log Messagem a ser mostrada no terminal
+   */
+  logger(log) {
+    let data = new Date();
+    let dia = this._dataZero(data.getDate());
+    let mes = this._dataZero(data.getMonth() + 1);
+    let ano = this._dataZero(data.getFullYear());
+    let hor = this._dataZero(data.getHours());
+    let min = this._dataZero(data.getMinutes());
+    let sec = this._dataZero(data.getSeconds());
 
-    /**
-     * @description Um logger simples que imprime no terminal ações
-     * realizadas pelo bot
-     * @param {String} log Messagem a ser mostrada no terminal
-     */
-    logger(log) {
-        let data = new Date();
-        let dia = this._dataZero(data.getDate());
-        let mes = this._dataZero(data.getMonth() + 1);
-        let ano = this._dataZero(data.getFullYear());
-        let hor = this._dataZero(data.getHours());
-        let min = this._dataZero(data.getMinutes());
-        let sec = this._dataZero(data.getSeconds());
+    let output = `[ ${dia}-${mes}-${ano} | ${hor}:${min}:${sec} ] ${log}`;
 
-        let output = `[ ${dia}-${mes}-${ano} | ${hor}:${min}:${sec} ] ${log}`;
+    console.log(output);
+  }
 
-        console.log(output);
+  /**
+   * @description Retorna todos os comandos aceitos pelo bot
+   * @param {Message} msg Objeto do comando enviado
+   * @param {string} prefix prefixo para reconhecer os comandos
+   * @param {Array} comandos lista de comandos atuais no bot
+   */
+  ajuda(msg, prefix, comandos) {
+    let resp = "Lista de comandos: ";
+    comandos.forEach(comando => {
+      resp += "\n" + prefix + comando;
+    });
+
+    msg.channel.send(resp);
+  }
+
+  /**
+   * @description Coleta as informações de uma build
+   * @param {Mensagem} msg Objeto da mensagem recebida
+   */
+  build(msg) {
+    let builds = require("./../data/builds.json");
+    let classe = msg.content.toLocaleLowerCase().split(" ")[1];
+    let response = "";
+    // TODO - pensar em uma forma de adicionar varias builds para
+    // uma mesma classe permitindo que
+    for (let i = 0; i < builds.length; i++) {
+      if (builds[i].classe == classe || builds[i].sigla == classe) {
+        response +=
+          "\n\n" +
+          builds[i].msg +
+          "\nForça:             " +
+          builds[i].forca +
+          "\nInteligência:  " +
+          builds[i].inteligencia +
+          "\nTalento:         " +
+          builds[i].talento +
+          "\nAgilidade:      " +
+          builds[i].agilidade +
+          "\nVitalidade:    " +
+          builds[i].vitalidade +
+          "\nBuild disponibilizada por " +
+          builds[i].autor;
+      }
     }
 
+    if (response == "") {
+      msg.reply(
+        "Informe a classe que você quer saber a build " +
+          "(archer, atalanta, kina, lutador, mago, mech, pike ou sacer)"
+      );
+    } else {
+      msg.author.send(response);
+    }
+  }
 
-    /**
-     * @description Retorna todos os comandos aceitos pelo bot
-     * @param {Message} msg Objeto do comando enviado
-     * @param {string} prefix prefixo para reconhecer os comandos
-     * @param {Array} comandos lista de comandos atuais no bot
-     */
-    ajuda(msg, prefix, comandos) {
-        let resp = 'Lista de comandos: ';
-        comandos.forEach(comando => {
-            resp += '\n' + prefix + comando;
-        });
+  /**
+   * @description recebe html com o rank, busca os 10 primeiros do rank
+   * e gera uma tabela contendo nick e número de kills
+   * @param {Mensagem} msg Objeto da mensagem recebida
+   */
+  ranking(msg) {
+    const uri = "http://user.monsterpk.com.br/scriptsnovosite/rankbp.php";
 
-        msg.channel.send(resp);
+    request(uri, (error, response, body) => {
+      let dados = this._limpaRanking(body);
+
+      let tabelaRank = this._montaTabelaRanking(dados);
+
+      msg.reply(tabelaRank);
+    });
+  }
+
+  /**
+   * @description Função paleativa para mostrar ranking individual. Será
+   * refatorada quando eu não tiver mais o que fazer :v
+   * @param {Mensagem} msg Objeto da mensagem recebida
+   */
+  rankIndividual(msg) {
+    let uri = "http://user.monsterpk.com.br/scriptsnovosite/rankbp.php";
+    let nick = msg.content.toLocaleLowerCase().split(" ")[1];
+    uri += "?nick=" + nick;
+
+    // TODO - Refatorar essa função para utilizar as funções de ranking
+    let a = request(uri, (error, response, body) => {
+      let nickL = body.match(/(?:td width="98".*>)(.+?)</gi);
+
+      if (!nickL) {
+        msg.reply("Não encontrei seu nick :/");
+        return;
+      }
+
+      nickL = nickL[0].match(/>(.*?)</gi)[0].slice(1, -1);
+      let killL = body
+        .match(/(?:td width="60".*>)(.+?)(?:<)/gi)[0]
+        .match(/>(.*?)</gi)[0]
+        .slice(1, -1);
+      let deathL = body
+        .match(/(?:td width="70".*>)(.+?)(?:<)/gi)[0]
+        .match(/>(.*?)</gi)[0]
+        .slice(1, -1);
+      let pointsL = body
+        .match(/(?:td width="80".*>)(.+?)(?:<)/gi)[0]
+        .match(/>(.*?)</gi)[0]
+        .slice(1, -1);
+
+      let nick = this._center(nickL, 22);
+      let kill = " " + this._addZero(killL, 3) + "  ";
+      let deat = "  " + this._addZero(deathL, 3) + "  ";
+      let pont = " " + this._addZero(pointsL, 4) + "  ";
+
+      let ranking =
+        "```\n" +
+        "╔══════════════════════╦═══════╦════════╦════════╗\n" +
+        "║         Nick         ║ Kills ║ Mortes ║ Pontos ║\n" +
+        "╠══════════════════════╬═══════╬════════╬════════╣\n" +
+        `║${nick}║${kill}║${deat}║${pont}║\n` +
+        "╚══════════════════════╩═══════╩════════╩════════╝\n" +
+        "```";
+
+      msg.reply(ranking);
+    });
+  }
+
+  pegaNoticia(msg) {
+    const urlNoticias =
+      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
+    request(urlNoticias, (error, response, body) => {
+      const codigoNoticia = body[Math.floor(Math.random() * body.length)];
+      const urlNoticia = `https://hacker-news.firebaseio.com/v0/item/${codigoNoticia}.json?print=pretty`;
+      request(urlNoticia, (error, response, body) => {
+        const noticia = `Titulo: ${body.title}\nPor: ${body.by}\nLink: ${body.url}`;
+        msg.reply(noticia);
+      });
+    });
+  }
+
+  /**
+   * @description Pega o Html bruto e devolve uma lista contendo 2 lista, uma
+   * com o nick dos 10 primeros do ranking e outra com as respequitivas kills
+   * de cada jogador
+   * @param {string} bruto string do html da pagina requisitada
+   * @returns {Array} Array com 4 arrays, um contendo nick do jogador, as
+   * kills desse jogador, as mortes e a quantidade de pontos
+   */
+  _limpaRanking(bruto) {
+    let res = [];
+    let nickLimpo = [];
+    let killLimpo = [];
+    let deathLimpo = [];
+    let pointLimpo = [];
+
+    let nicks = bruto.match(/(?:td width="98".*>)(.+?)</gi);
+    let kills = bruto.match(/(?:td width="60".*>)(.+?)(?:<)/gi);
+    let deaths = bruto.match(/(?:td width="70".*>)(.+?)(?:<)/gi);
+    let points = bruto.match(/(?:td width="80".*>)(.+?)(?:<)/gi);
+
+    for (let i = 0; i < 10; i++) {
+      let ni = nicks[i].match(/>(.*?)</gi)[0].slice(1, -1);
+      let ki = kills[i].match(/>(.*?)</gi)[0].slice(1, -1);
+      let de = deaths[i].match(/>(.*?)</gi)[0].slice(1, -1);
+      let po = points[i].match(/>(.*?)</gi)[0].slice(1, -1);
+
+      nickLimpo.push(ni);
+      killLimpo.push(ki);
+      deathLimpo.push(de);
+      pointLimpo.push(po);
     }
 
+    res.push(nickLimpo);
+    res.push(killLimpo);
+    res.push(deathLimpo);
+    res.push(pointLimpo);
 
-    /**
-     * @description Coleta as informações de uma build
-     * @param {Mensagem} msg Objeto da mensagem recebida
-     */
-    build(msg) {
-        let builds = require('./../data/builds.json');
-        let classe = msg.content.toLocaleLowerCase().split(' ')[1];
-        let response = '';
-        // TODO - pensar em uma forma de adicionar varias builds para
-        // uma mesma classe permitindo que 
-        for (let i = 0; i < builds.length; i++) {
-            if (builds[i].classe == classe || builds[i].sigla == classe) {
-                response += '\n\n' +
-                    builds[i].msg +
-                    '\nForça:             ' + builds[i].forca +
-                    '\nInteligência:  ' + builds[i].inteligencia +
-                    '\nTalento:         ' + builds[i].talento +
-                    '\nAgilidade:      ' + builds[i].agilidade +
-                    '\nVitalidade:    ' + builds[i].vitalidade +
-                    '\nBuild disponibilizada por ' + builds[i].autor;
-            }
-        }
+    return res;
+  }
 
-        if (response == '') {
-            msg.reply('Informe a classe que você quer saber a build ' +
-                '(archer, atalanta, kina, lutador, mago, mech, pike ou sacer)');
-        } else {
-            msg.author.send(response);
-        }
+  /**
+   * @description recebe o nick e as kills dos 10 primeiro jogadores
+   * e cria uma tabela estilizada com essas informações
+   * @param {Array} dados Array com 4 arrays, um contendo nick do
+   * jogador, as kills desse jogador, as mortes e a quantidade de pontos
+   * @returns {string} Tabela dos 10 primeiros do ranking
+   */
+  _montaTabelaRanking(dados) {
+    const nicks = dados[0];
+    const kills = dados[1];
+    const death = dados[2];
+    const points = dados[3];
+
+    let ranking =
+      "```\n" +
+      "╔══════════════════════════════════════════════════════════╗\n" +
+      "║                      MESTRES DO PVP                      ║\n" +
+      "╠═════════╦══════════════════════╦═══════╦════════╦════════╣\n" +
+      "║ Posição ║         Nick         ║ Kills ║ Mortes ║ Pontos ║\n" +
+      "╠═════════╬══════════════════════╬═══════╬════════╬════════╣\n";
+
+    for (let i = 0; i < 10; i++) {
+      let rank = "   " + this._addZero(i + 1, 2) + "   ";
+      let nick = this._center(nicks[i], 22);
+      let kill = " " + this._addZero(kills[i], 3) + "  ";
+      let deat = "  " + this._addZero(death[i], 3) + "  ";
+      let pont = " " + this._addZero(points[i], 4) + "  ";
+
+      ranking += `║${rank}║${nick}║${kill}║${deat}║${pont}║\n`;
     }
 
+    ranking +=
+      "╚═════════╩══════════════════════╩═══════╩════════╩════════╝\n" + "```";
 
-    /**
-     * @description recebe html com o rank, busca os 10 primeiros do rank
-     * e gera uma tabela contendo nick e número de kills
-     * @param {Mensagem} msg Objeto da mensagem recebida
-     */
-    ranking(msg) {
-        const uri = 'http://user.monsterpk.com.br/scriptsnovosite/rankbp.php';
+    return ranking;
+  }
 
-        request(uri, (error, response, body) => {
-            let dados = this._limpaRanking(body);
+  /**
+   * @description Centraliza uma palavra em uma certa quantidade de caracteres
+   * @param {String} word palavra a ser centralizada
+   * @param {int} size tamanho total para centralizar a palavra
+   * @returns {String} espaço $word espaço
+   */
+  _center(word, size) {
+    let res = "";
+    let val = size - word.length;
+    let space = Math.floor(val / 2);
 
-            let tabelaRank = this._montaTabelaRanking(dados);
+    res += val % 2 != 0 ? " " : "";
+    res += " ".repeat(space) + word + " ".repeat(space);
 
-            msg.reply(tabelaRank);
-        });
+    return res;
+  }
+
+  /**
+   * @description Adiciona 0's a esquerda do valor fornecido para igualar
+   * ao tamanho desejado
+   * @param {int} val valor a ser validado
+   * @param {int} size tamanho que valor deveria ter
+   * @returns {String} valor adaptado ou não
+   */
+  _addZero(val, size) {
+    let minus = " ";
+    if (val < 0) {
+      minus = "-";
+      val = Math.abs(val);
     }
 
-
-    /**
-     * @description Função paleativa para mostrar ranking individual. Será 
-     * refatorada quando eu não tiver mais o que fazer :v
-     * @param {Mensagem} msg Objeto da mensagem recebida
-     */
-    rankIndividual(msg) {
-        let uri = 'http://user.monsterpk.com.br/scriptsnovosite/rankbp.php';
-        let nick = msg.content.toLocaleLowerCase().split(" ")[1];
-        uri += '?nick=' + nick;
-
-        // TODO - Refatorar essa função para utilizar as funções de ranking
-        let a = request(uri, (error, response, body) => {
-            let nickL = body.match(/(?:td width="98".*>)(.+?)</ig)
-
-            if (!nickL) {
-                msg.reply('Não encontrei seu nick :/');
-                return;
-            }
-
-            nickL = nickL[0].match(/>(.*?)</ig)[0].slice(1, -1);
-            let killL = body.match(/(?:td width="60".*>)(.+?)(?:<)/ig)[0]
-                .match(/>(.*?)</ig)[0].slice(1, -1);
-            let deathL = body.match(/(?:td width="70".*>)(.+?)(?:<)/ig)[0]
-                .match(/>(.*?)</ig)[0].slice(1, -1);
-            let pointsL = body.match(/(?:td width="80".*>)(.+?)(?:<)/ig)[0]
-                .match(/>(.*?)</ig)[0].slice(1, -1);
-
-            let nick = this._center(nickL, 22)
-            let kill = ' ' + this._addZero(killL, 3) + '  ';
-            let deat = '  ' + this._addZero(deathL, 3) + '  ';
-            let pont = ' ' + this._addZero(pointsL, 4) + '  ';
-
-            let ranking = '\`\`\`\n' +
-                '╔══════════════════════╦═══════╦════════╦════════╗\n' +
-                '║         Nick         ║ Kills ║ Mortes ║ Pontos ║\n' +
-                '╠══════════════════════╬═══════╬════════╬════════╣\n' +
-                `║${nick}║${kill}║${deat}║${pont}║\n` +
-                '╚══════════════════════╩═══════╩════════╩════════╝\n' +
-                '\`\`\`';
-
-            msg.reply(ranking);
-        });
-
+    let res = "" + val;
+    if (res.length < size) {
+      res = "0".repeat(size - res.length) + res;
     }
+    return minus + res;
+  }
 
-
-    /**
-     * @description Pega o Html bruto e devolve uma lista contendo 2 lista, uma 
-     * com o nick dos 10 primeros do ranking e outra com as respequitivas kills 
-     * de cada jogador
-     * @param {string} bruto string do html da pagina requisitada
-     * @returns {Array} Array com 4 arrays, um contendo nick do jogador, as 
-     * kills desse jogador, as mortes e a quantidade de pontos
-     */
-    _limpaRanking(bruto) {
-        let res = [];
-        let nickLimpo = [];
-        let killLimpo = [];
-        let deathLimpo = [];
-        let pointLimpo = [];
-
-        let nicks = bruto.match(/(?:td width="98".*>)(.+?)</ig);
-        let kills = bruto.match(/(?:td width="60".*>)(.+?)(?:<)/ig);
-        let deaths = bruto.match(/(?:td width="70".*>)(.+?)(?:<)/ig);
-        let points = bruto.match(/(?:td width="80".*>)(.+?)(?:<)/ig);
-
-        for (let i = 0; i < 10; i++) {
-            let ni = nicks[i].match(/>(.*?)</ig)[0].slice(1, -1);
-            let ki = kills[i].match(/>(.*?)</ig)[0].slice(1, -1);
-            let de = deaths[i].match(/>(.*?)</ig)[0].slice(1, -1);
-            let po = points[i].match(/>(.*?)</ig)[0].slice(1, -1);
-
-            nickLimpo.push(ni);
-            killLimpo.push(ki);
-            deathLimpo.push(de);
-            pointLimpo.push(po);
-        }
-
-        res.push(nickLimpo);
-        res.push(killLimpo);
-        res.push(deathLimpo);
-        res.push(pointLimpo);
-
-        return res;
-    }
-
-
-    /**
-     * @description recebe o nick e as kills dos 10 primeiro jogadores 
-     * e cria uma tabela estilizada com essas informações
-     * @param {Array} dados Array com 4 arrays, um contendo nick do
-     * jogador, as kills desse jogador, as mortes e a quantidade de pontos
-     * @returns {string} Tabela dos 10 primeiros do ranking
-     */
-    _montaTabelaRanking(dados) {
-        const nicks = dados[0];
-        const kills = dados[1];
-        const death = dados[2];
-        const points = dados[3];
-
-        let ranking = '\`\`\`\n' +
-            '╔══════════════════════════════════════════════════════════╗\n' +
-            '║                      MESTRES DO PVP                      ║\n' +
-            '╠═════════╦══════════════════════╦═══════╦════════╦════════╣\n' +
-            '║ Posição ║         Nick         ║ Kills ║ Mortes ║ Pontos ║\n' +
-            '╠═════════╬══════════════════════╬═══════╬════════╬════════╣\n'
-
-        for (let i = 0; i < 10; i++) {
-
-            let rank = '   ' + this._addZero(i + 1, 2) + '   ';
-            let nick = this._center(nicks[i], 22)
-            let kill = ' ' + this._addZero(kills[i], 3) + '  ';
-            let deat = '  ' + this._addZero(death[i], 3) + '  ';
-            let pont = ' ' + this._addZero(points[i], 4) + '  ';
-
-            ranking += `║${rank}║${nick}║${kill}║${deat}║${pont}║\n`
-        }
-
-        ranking +=
-            '╚═════════╩══════════════════════╩═══════╩════════╩════════╝\n' +
-            '\`\`\`';
-
-        return ranking;
-    }
-
-    /**
-     * @description Centraliza uma palavra em uma certa quantidade de caracteres
-     * @param {String} word palavra a ser centralizada
-     * @param {int} size tamanho total para centralizar a palavra
-     * @returns {String} espaço $word espaço
-     */
-    _center(word, size) {
-        let res = '';
-        let val = size - word.length;
-        let space = Math.floor(val / 2);
-
-        res += (val % 2 != 0) ? ' ' : '';
-        res += ' '.repeat(space) + word + ' '.repeat(space);
-
-        return res;
-    }
-
-
-    /**
-     * @description Adiciona 0's a esquerda do valor fornecido para igualar
-     * ao tamanho desejado
-     * @param {int} val valor a ser validado
-     * @param {int} size tamanho que valor deveria ter
-     * @returns {String} valor adaptado ou não
-     */
-    _addZero(val, size) {
-        let minus = ' ';
-        if (val < 0) {
-            minus = '-';
-            val = Math.abs(val);
-        }
-
-        let res = '' + val;
-        if (res.length < size) {
-            res = '0'.repeat(size - res.length) + res;
-        }
-        return minus + res
-    }
-
-    /**
-     * @description Adiciona 0 a esquerda do valor fornecido para ter 2 digitos
-     * @param {int} num valor a ser editado
-     * @returns 0n ou n
-     */
-    _dataZero(num) {
-        return (num < 10) ? '0' + num : num;
-    }
+  /**
+   * @description Adiciona 0 a esquerda do valor fornecido para ter 2 digitos
+   * @param {int} num valor a ser editado
+   * @returns 0n ou n
+   */
+  _dataZero(num) {
+    return num < 10 ? "0" + num : num;
+  }
 }
 
 module.exports = Acoes;
